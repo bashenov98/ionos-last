5b47ba46efde24b3251d575818460c8b2ac4a7e2
madibarca <abzhanma@gmail.com>
2023-10-16 01:19:56 +0600

    header mobile added

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -8,6 +8,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import headerData from "./header.data";
 import languagesData from "./languages.data";
 
+import { Button, Drawer } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+
 import YoutubeIcon from "../../media/social/youtube-fill.svg";
 import InstagramIcon from "../../media/social/instagram-fill.svg";
 import FacebookIcon from "../../media/social/facebook-box-fill.svg";
@@ -22,95 +25,207 @@ const Header = () => {
     i18n.changeLanguage(lng);
   };
 
+  const windowWidth = window.innerWidth;
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const showDrawer = () => {
+    setDrawerOpen(true);
+  };
+
+  const onMouseLeaveMobile = () => {
+    setDropdownVisibility(0);
+    setDrawerOpen(false);
+  };
+
   return (
     <div className="header">
-      <div className="headerTop">
-        <div className="headerTopContacts">
-          <Link
-            style={{ paddingRight: "18px" }}
-            to="https://youtube.com/@Instituteofionosphere?si=Ql_V8WJWj0ItWEO9"
-          >
-            <img src={YoutubeIcon} />
-          </Link>
-          <Link
-            style={{ paddingRight: "18px" }}
-            to="https://instagram.com/ionoskz?igshid=MzRlODBiNWFlZA=="
-          >
-            <img src={InstagramIcon} />
-          </Link>
-          <Link
-            style={{ paddingRight: "18px" }}
-            to="https://www.linkedin.com/company/ionos-kz/"
-          >
-            <img src={LinkedInIcon} />
-          </Link>
-          <Link to="https://m.facebook.com/p/100081835389053/">
-            <img src={FacebookIcon} />
-          </Link>
+      {windowWidth > 1080 && (
+        <div className="headerTop">
+          <div className="headerTopContacts">
+            <Link
+              style={{ paddingRight: "18px" }}
+              to="https://youtube.com/@Instituteofionosphere?si=Ql_V8WJWj0ItWEO9"
+            >
+              <img src={YoutubeIcon} />
+            </Link>
+            <Link
+              style={{ paddingRight: "18px" }}
+              to="https://instagram.com/ionoskz?igshid=MzRlODBiNWFlZA=="
+            >
+              <img src={InstagramIcon} />
+            </Link>
+            <Link
+              style={{ paddingRight: "18px" }}
+              to="https://www.linkedin.com/company/ionos-kz/"
+            >
+              <img src={LinkedInIcon} />
+            </Link>
+            <Link to="https://m.facebook.com/p/100081835389053/">
+              <img src={FacebookIcon} />
+            </Link>
+          </div>
+          <div className="headerTopLinks">
+            <Link
+              className="headerTopLink"
+              to="https://www.gov.kz/memleket/entities/kazcosmos/about?lang=ru"
+            >
+              {t("committee")}
+            </Link>
+          </div>
+          <div className="headerLanguageButtons">
+            {languagesData.map((item, i) => (
+              <a onClick={() => changeLanguage(`${item}`)}>
+                <div className="headerLanguageButton">
+                  <h1 className="headerLanguageButtonText" key={i}>
+                    {item.toLocaleUpperCase()}
+                  </h1>
+                </div>
+              </a>
+            ))}
+          </div>
         </div>
-        <div className="headerTopLinks">
-          <Link
-            className="headerTopLink"
-            to="https://www.gov.kz/memleket/entities/kazcosmos/about?lang=ru"
-          >
-            {t("committee")}
-          </Link>
-        </div>
-        <div className="headerLanguageButtons">
-          {languagesData.map((item, i) => (
-            <a onClick={() => changeLanguage(`${item}`)}>
-              <div className="headerLanguageButton">
-                <h1 className="headerLanguageButtonText" key={i}>
-                  {item.toLocaleUpperCase()}
-                </h1>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
+      )}
       <div className="headerBot">
         <div className="headerLogoDiv">
           <Link to="/">
             <img src={Logo} className="headerLogo" alt="logo" />
           </Link>
         </div>
-        <div style={{ width: "100%" }}>
-          <nav>
-            <ul className="headerBotTabs">
+
+        {windowWidth > 1080 ? (
+          <div style={{ width: "100%" }}>
+            <nav>
+              <ul className="headerBotTabs">
+                {headerData.map((item, i) => (
+                  <li
+                    onMouseEnter={() => setDropdownVisibility(item.index)}
+                    onMouseLeave={() => setDropdownVisibility(0)}
+                    key={i}
+                  >
+                    <a class="dropdown-toggle" className="headerBotTab">
+                      {t(item.section)}
+                    </a>
+                    <ul
+                      class="dropdown-menu no-border-radius fadeIn animated"
+                      style={{
+                        display:
+                          isDropdownVisible === item.index ? "block" : "none",
+                        marginLeft: i === 2 ? "0px" : i === 3 && "-570px",
+                      }}
+                    >
+                      {item.subSection.map((subsection, i) => (
+                        <li className="droptab" key={i}>
+                          <Link
+                            className="droptab-link"
+                            to={`/${item.section}/${subsection}`}
+                            key={i}
+                          >
+                            {t(subsection)}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        ) : (
+          <div style={{ width: "100%" }} className="drawerMobile">
+            <Button
+              type="primary"
+              onClick={showDrawer}
+              size="large"
+              style={{ marginLeft: "100%", background: "white" }}
+            >
+              <MenuOutlined
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "black",
+                }}
+              />
+            </Button>
+            <Drawer
+              placement="right"
+              onClose={() => setDrawerOpen(false)}
+              open={drawerOpen}
+              width={"80%"}
+            >
+              <div className="headerLanguageButtons">
+                {languagesData.map((item, i) => (
+                  <a onClick={() => changeLanguage(`${item}`)}>
+                    <div className="headerLanguageButton">
+                      <h1 className="headerLanguageButtonText" key={i}>
+                        {item.toLocaleUpperCase()}
+                      </h1>
+                    </div>
+                  </a>
+                ))}
+              </div>
               {headerData.map((item, i) => (
-                <li
-                  onMouseEnter={() => setDropdownVisibility(item.index)}
-                  onMouseLeave={() => setDropdownVisibility(0)}
-                  key={i}
-                >
-                  <a class="dropdown-toggle" className="headerBotTab">
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <a
+                    key={i}
+                    style={{
+                      fontSize: "40px",
+                      fontWeight: "500",
+                      marginTop: "30px",
+                    }}
+                    onMouseEnter={() => setDropdownVisibility(item.index)}
+                    onMouseLeave={onMouseLeaveMobile}
+                  >
                     {t(item.section)}
                   </a>
-                  <ul
-                    class="dropdown-menu no-border-radius fadeIn animated"
-                    style={{
-                      display:
-                        isDropdownVisible === item.index ? "block" : "none",
-                      marginLeft: i === 2 ? "0px" : i === 3 && "-570px",
-                    }}
-                  >
-                    {item.subSection.map((subsection, i) => (
-                      <li className="droptab" key={i}>
-                        <Link
-                          className="droptab-link"
-                          to={`/${item.section}/${subsection}`}
-                          key={i}
-                        >
-                          {t(subsection)}
-                        </Link>
-                      </li>
+                  {isDropdownVisible === item.index &&
+                    item.subSection.map((subsection, i) => (
+                      <Link
+                        className="droptab-link"
+                        to={`/${item.section}/${subsection}`}
+                        key={i}
+                      >
+                        {t(subsection)}
+                      </Link>
                     ))}
-                  </ul>
-                </li>
+                </div>
               ))}
-            </ul>
-          </nav>
-        </div>
+
+              <div className="headerTopContacts">
+                <Link
+                  style={{ paddingRight: "18px" }}
+                  to="https://youtube.com/@Instituteofionosphere?si=Ql_V8WJWj0ItWEO9"
+                >
+                  <img src={YoutubeIcon} />
+                </Link>
+                <Link
+                  style={{ paddingRight: "18px" }}
+                  to="https://instagram.com/ionoskz?igshid=MzRlODBiNWFlZA=="
+                >
+                  <img src={InstagramIcon} />
+                </Link>
+                <Link
+                  style={{ paddingRight: "18px" }}
+                  to="https://www.linkedin.com/company/ionos-kz/"
+                >
+                  <img src={LinkedInIcon} />
+                </Link>
+                <Link to="https://m.facebook.com/p/100081835389053/">
+                  <img src={FacebookIcon} />
+                </Link>
+              </div>
+              <div className="headerTopLinks">
+                <Link
+                  className="headerTopLink"
+                  to="https://www.gov.kz/memleket/entities/kazcosmos/about?lang=ru"
+                >
+                  {t("committee")}
+                </Link>
+              </div>
+            </Drawer>
+          </div>
+        )}
       </div>
     </div>
   );
