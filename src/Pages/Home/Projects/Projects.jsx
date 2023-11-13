a053e318e13cb87bac41f9b5df4f1b2833df2a2f
madibarca <abzhanma@gmail.com>
2023-11-14 01:06:01 +0600

    header fixed

--- src/Pages/Home/Projects/Projects.jsx
+++ src/Pages/Home/Projects/Projects.jsx
@@ -1,59 +1,59 @@
 import React, { useEffect } from "react";
 
-import './Projects.css';
+import "./Projects.css";
 
-import maxm from '../../../media/projects/maxm.png';
-import planet from '../../../media/projects/planet.png';
-import landslides from '../../../media/projects/landslides.png';
+import maxm from "../../../media/projects/maxm.png";
+import planet from "../../../media/projects/planet.png";
+import landslides from "../../../media/projects/landslides.png";
 
-import logo from '../../../media/projects/ionos-logo.png'
+import logo from "../../../media/projects/ionos-logo.png";
 
 export const Projects = () => {
-    const projects = [
-        {
-            id: 1,
-            img: maxm,
-            header: "«МАКСМ» Многоцелевая аэрокосмическая система мониторинга",
-        },
-        {
-            id: 2,
-            img: planet,
-            header: "Разработка системы мониторинга за сдвижением земной поверхности на основе данных ДЗЗ",
-        },
-        {
-            id: 3,
-            img: landslides,
-            header: "Разработка интеллектуальной системы прогнозирования оползневых процессов",
-        },
-    ];
+  const projects = [
+    {
+      id: 1,
+      img: maxm,
+      header: "«МАКСМ» Многоцелевая аэрокосмическая система мониторинга",
+    },
+    {
+      id: 2,
+      img: planet,
+      header:
+        "Разработка системы мониторинга за сдвижением земной поверхности на основе данных ДЗЗ",
+    },
+    {
+      id: 3,
+      img: landslides,
+      header:
+        "Разработка интеллектуальной системы прогнозирования оползневых процессов",
+    },
+  ];
 
-
-
-    return (
-        <div className="projectsContainer">
-            <div className="projectsHeader">
-                <h1 className="projectsHeaderText">Выполняемые проекты</h1>
+  return (
+    <div className="projectsContainer">
+      <div className="projectsHeader">
+        <h1 className="projectsHeaderText">Выполняемые проекты</h1>
+      </div>
+      <div className="projectList">
+        {projects.map((project, i) => (
+          <div className="projectItem" key={i}>
+            <div className="projectItemTop">
+              <div className="projectIonosLogo">
+                <img className="projectIonosLogoImg" src={logo} />
+              </div>
+              <div className="projectLogo">
+                <img className="projectLogoImg" src={project.img} />
+              </div>
             </div>
-            <div className="projectList">
-                {projects.map((project, i) => (
-                    <div className="projectItem">
-                        <div className="projectItemTop">
-                            <div className="projectIonosLogo">
-                                <img className="projectIonosLogoImg" src={logo} />
-                            </div>
-                            <div className="projectLogo">
-                                <img className="projectLogoImg" src={project.img} />
-                            </div>
-                        </div>
-                        <div className="projectItemBot">
-                            <h3 className="projectItemBotText">{project.header}</h3>
-                        </div>
-                    </div>
-                ))}
+            <div className="projectItemBot">
+              <h3 className="projectItemBotText">{project.header}</h3>
             </div>
-            <div className="projectsButtonDiv">
-                <button className="projectsButton">ВСЕ ПРОЕКТЫ</button>
-            </div>
-        </div>
-    );
-}
+          </div>
+        ))}
+      </div>
+      <div className="projectsButtonDiv">
+        <button className="projectsButton">ВСЕ ПРОЕКТЫ</button>
+      </div>
+    </div>
+  );
+};
